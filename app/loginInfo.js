bbfff5178eb9851f5b96470dca2d1ca8092ec811
kjih1104 <kjih114@gmail.com>
2024-04-25 15:15:08 +0900

    간이 로그인

---
+++ app/loginInfo.js
@@ -0,0 +1,4 @@
+let storeID = "weq";
+let storePW = "e567e79b8d09de883e3ec25a09ac556dc04352074d67b65b0f1b744aa4c360ac";
+
+export {storeID, storePW};
--- app/page.js
+++ app/page.js
@@ -1,14 +1,13 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div>
       <h4>dcmall</h4>
 
-      <form>
-        <input>ID : </input>
-        <input>PW : </input>
+      <form action="/api/post/login" method = "POST">
+        <input type="text" placeholder="ID" name="id" />
+        <input type="password" placeholder="PW" name="password" />
+        <button type="submit">login</button>
       </form>
     </div>
   );
