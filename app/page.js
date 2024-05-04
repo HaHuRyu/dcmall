5b3b6d3faf29abfa7dc77a329dcd97fe4952a167
HaHuRyu <dlwjdgml4567@gmail.com>
2024-05-04 19:40:23 +0900

    충돌 해결

--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
-import {id, password} from "./login"
 
 export default function Home() {
   return (
     <div>
-      <h4>이정희 꺼임</h4>
       <h4>dcmall</h4>
 
       <form action="/api/post/login" method = "POST">
