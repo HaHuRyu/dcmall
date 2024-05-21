323b5bdf6ec659cc17a2cf43393cfc2617138806
kjih1104 <kjih114@gmail.com>
2024-05-21 16:18:20 +0900

    jwt 관련 설명 추가 (삭제해도 됨)

--- app/api/auth/[...nextauth].js
+++ app/api/auth/[...nextauth].js
@@ -12,7 +12,13 @@ export default NextAuth({
         id: { label: "ID", type: "text" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials) {
+      // 설명: 원래의 토큰 방식에서는 토큰을 발급하여 클라이언트의 쿠키에 저장하고,
+      // 동시에 서버에서 그 토큰을 데이터베이스에 저장하여 이후 요청에서 비교하는 방식이다.
+      // 하지만, JWT는 자체적으로 서명된 토큰이기 때문에 서버 측 데이터베이스 조회 없이도
+      // 토큰의 유효성을 확인할 수 있다. 따라서 JWT를 사용하면 로그인 시 데이터베이스에서
+      // 사용자 ID와 비밀번호를 확인하고, 이후에는 토큰만 검증하면 된다. 이 authorize 함수에서는
+      // 이미 DB에서 ID와 비밀번호를 확인했다고 가정하고, JWT 토큰만 발급하여 반환한다.
+      async authorize(credentials) {  //POST여야만 한다
         // 사용자 인증 로직
         console.log("authorize!!!");
         // 예시로 간단하게 처리 - 실제로는 데이터베이스 등에서 사용자 인증 로직을 수행
@@ -27,6 +33,7 @@ export default NextAuth({
           return null;
         }
       }
+
     })
   ],
   callbacks: {
