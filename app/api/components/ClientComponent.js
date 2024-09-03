da2b51950592ae61aa636006800c5581c42a728d
kjih1104 <kjih114@gmail.com>
2024-09-03 15:25:01 +0900

    kjih와 로그인 관련 merge 하기 전 마지막 커밋

--- app/api/components/ClientComponent.js
+++ app/api/components/ClientComponent.js
@@ -14,7 +14,6 @@ export default function ClientComponent() {
   const [resultList, setResultList] = useState([]);
   const [allProductList, setAllProductList] = useState([]);
   const [renderTrigger, setRenderTrigger] = useState(false);
-  const { data: session } = useSession();
  
   const fetchAllProducts = async (e) => {
     try{
@@ -70,63 +69,6 @@ export default function ClientComponent() {
     }
   }, [resultList]);
 
-  useEffect(() => {
-    if (session) {
-      if (session.provider === 'google') {
-        googleSignIn(session); 
-      }
-  
-      console.log("확인: " + JSON.stringify(session));
-      sessionRegist(session);
-    }
-  }, [session]);
-  
-
-  const sessionRegist = async (session) =>{
-    await fetch('/api/post/sessionRegist', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email: session?.user?.email,
-        provider: session?.provider,
-      })
-    })
-  }
-
-  const googleSignIn = async (session) => {
-    try {
-      const response = await fetch('/api/post/joinServer/google', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email: session?.user?.email, // 최신 세션 값을 사용합니다.
-        }),
-      });
-  
-      const data = await response.json();
-  
-      if (response.status === 200) {
-        console.log('구글 로그인 성공');
-      } else if (response.status === 201) {
-        sessionStorage.setItem('userEmail', session?.user?.email || '');
-        window.location.href = data.url;
-      }
-    } catch (error) {
-      console.log("Google SignIn Error: " + error);
-    }
-  };
- 
-  const handleLogOut = async () => {
-    await signOut({ redirect: false });
-    sessionStorage.clear(); // 세션 스토리지를 명시적으로 클리어
-    await new Promise(resolve => setTimeout(resolve, 500)); // 0.5초 정도 기다리기
-    window.location.reload();
-  };
-
   const handleBlur = async (e) => { //자동완성 같은 기능
     e.preventDefault();
     try {
@@ -158,35 +100,7 @@ export default function ClientComponent() {
   return (
     <div>
       <p>Dcmall</p>
-      {!session ? ( //signIn안에 'google or credentials'로 지정해주면 문자열+Provider 방식의 로그인을 한다는 의미 문자열로 커스텀 구글을 나눈다
-        <>
-          <button onClick={() => signIn('google', {redirect: false})}>구글 로그인</button>
-          <form onSubmit={async (e) => {
-            e.preventDefault();
-            const email = e.target.email.value;
-            const password = e.target.password.value;
-            const result = await signIn('credentials', { redirect: false, email, password }); //Credentials(커스텀 로그인)에 로그인을 요청 리다이렉트 금지, email, password를 authorize에 보낸다.
-            if (result?.error) {
-              console.log("커스텀 로그인 실패:", result.error);
-            }
-          }}>
-            <input name="email" type="text" placeholder="Email" />
-            <input name="password" type="password" placeholder="Password" />
-            <button type="submit">커스텀 로그인</button>
-          </form>
-        </>
-      ) : (
-        <button onClick={() => handleLogOut()}>로그아웃</button>
-      )}
-      
-      {session ? (
-        <div>
-          <p>Name: {session.user.name}</p>
-          <p>Email: {session.user.email}</p>
-        </div>
-      ) : (
-        <p>Please sign in to see your profile</p>
-      )}
+      <a href="/login/signIn"><button>로그인</button></a>
 
       <form onSubmit={searchSubmit}>
         <input 
--- app/login/signIn/page.js
+++ app/login/signIn/page.js
@@ -1,59 +1,100 @@
 'use client'
-import React, { useState } from 'react';
-import { XSS_Sanitize } from '../../util/xssSanitize';
+import React, { useEffect } from 'react';
+import{ useSession, signIn, signOut} from 'next-auth/react';
 
 export default function SignIn() {
-  const [inputID, setInputID] = useState('');
-  const [sanitizedOutputID, setSanitizedOutputID] = useState('');
-  const [inputPW, setInputPW] = useState('');
-  const [sanitizedOutputPW, setSanitizedOutputPW] = useState('');
+  const { data: session } = useSession();
+ 
+  useEffect(() => {
+    if (session) {
+      if (session.provider === 'google') {
+        googleSignIn(session); 
+      }
+  
+      console.log("확인: " + JSON.stringify(session));
+      sessionRegist(session);
+    }
+  }, [session]);
+  
 
-  const handleSubmit = async (e) =>{
-    e.preventDefault();
-    try{
-      const response = await fetch('/api/post/login', {
+  const sessionRegist = async (session) =>{
+    await fetch('/api/post/sessionRegist', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: session?.user?.email,
+        provider: session?.provider,
+      })
+    })
+  }
+
+  const googleSignIn = async (session) => {
+    try {
+      const response = await fetch('/api/post/joinServer/google', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ 
-          id: sanitizedOutputID,
-          password: sanitizedOutputPW
+        body: JSON.stringify({
+          email: session?.user?.email, // 최신 세션 값을 사용합니다.
         }),
       });
-      const res = await response.json();
-      alert(res.message);
-      
-      if(response.status === 200){
-        window.location.href = '/';
+  
+      const data = await response.json();
+  
+      if (response.status === 200) {
+        console.log('구글 로그인 성공');
+      } else if (response.status === 201) {
+        sessionStorage.setItem('userEmail', session?.user?.email || '');
+        window.location.href = data.url;
       }
-    }catch (error) {
-      console.error('Login failed:', error);
+    } catch (error) {
+      console.log("Google SignIn Error: " + error);
     }
+  };
+ 
+  const handleLogOut = async () => {
+    await signOut({ redirect: false });
+    sessionStorage.clear(); // 세션 스토리지를 명시적으로 클리어
+    await new Promise(resolve => setTimeout(resolve, 500)); // 0.5초 정도 기다리기
+    window.location.reload();
   };
 
   return (
     <div>
       <h4>dcmall</h4>
 
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="ID"
-          name="id"
-          id="id"
-          value={sanitizedOutputID}
-          onChange={XSS_Sanitize(setInputID, setSanitizedOutputID)}
-        />
-        <input
-          type="password"
-          placeholder="PW"
-          name="password"
-          value={sanitizedOutputPW}
-          onChange={XSS_Sanitize(setInputPW, setSanitizedOutputPW)}
-        />
-        <button type="submit">로그인</button>
-      </form>
+      {!session ? ( //signIn안에 'google or credentials'로 지정해주면 문자열+Provider 방식의 로그인을 한다는 의미 문자열로 커스텀 구글을 나눈다
+        <>
+          <button onClick={() => signIn('google', {redirect: false})}>구글 로그인</button>
+          <form onSubmit={async (e) => {
+            e.preventDefault();
+            const email = e.target.email.value;
+            const password = e.target.password.value;
+            const result = await signIn('credentials', { redirect: false, email, password }); //Credentials(커스텀 로그인)에 로그인을 요청 리다이렉트 금지, email, password를 authorize에 보낸다.
+            if (result?.error) {
+              console.log("커스텀 로그인 실패:", result.error);
+            }
+          }}>
+            <input name="email" type="text" placeholder="Email" />
+            <input name="password" type="password" placeholder="Password" />
+            <button type="submit">커스텀 로그인</button>
+          </form>
+        </>
+      ) : (
+        <button onClick={() => handleLogOut()}>로그아웃</button>
+      )}
+      
+      {session ? (
+        <div>
+          <p>Name: {session.user.name}</p>
+          <p>Email: {session.user.email}</p>
+        </div>
+      ) : (
+        <p>Please sign in to see your profile</p>
+      )}
 
       <a href="/login/join"><button>회원가입</button></a>
       <a href="/login/findID"><button>ID 찾기</button></a>
